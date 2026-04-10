fb177506c83b4e81ae4eaac24e536612dcad2018
anjal <ganeswaridamisetti@gmail.com>
2026-04-10 09:51:29 +0530

    commit

---
+++ src/com/javaintro/Literals.java
@@ -0,0 +1,31 @@
+package com.javaintro;
+
+public class Literals {
+
+	public static void main(String[] args) {
+		int a1=123;
+		int a2=1230;
+		int a3=0123;
+		int a4=0x345;
+		int a5=0xdad;
+		int a6=0b01101;
+		String s1="srikanth";
+		String s2=new String();
+		String s3=new String("java");
+		String s4=new String("java");
+		String s5="srikanth";
+		System.out.println(a1);
+		System.out.println(a2);
+		System.out.println(a3);
+		System.out.println(a4);
+		System.out.println(a5);
+		System.out.println(a6);
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+		System.out.println(s3==s4);
+		System.out.println(s1==s5);
+
+	}
+
+}
